600f1089bfa098a8584e7df66a320ef4f7aa9a56
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-08-07 12:38:52 +0900

    delete modules for db

--- src/app.py
+++ src/app.py
@@ -2,14 +2,11 @@
 from flask import Flask
 # import CORS for front and back
 from flask_cors import CORS
-# import MySQL for DB
-from flask_mysqldb import MySQL
-from dotenv import load_dotenv
+
 
 # create Flask class
 app = Flask(__name__)
 CORS(app)
-mysql = MySQL(app)
 
 
 @app.route('/')
